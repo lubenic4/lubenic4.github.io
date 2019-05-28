6030fdd5c7fe7abe76b0ed4f42a1a60633d7204e
lubenic4 <zinedin.mezit@edu.fit.ba>
2019-05-28 23:11:04 +0200

    nav animation

---
+++ Portfolio/skripta.js
@@ -0,0 +1,21 @@
+window.onscroll=function()
+{
+    var navig = document.getElementById('navigacija');
+    var opcije=document.getElementsByClassName('opcije');
+    if(window.pageYOffset>35)
+    {
+        navig.classList.add("navigacija2");
+        for(var i=0;i<opcije.length;i++)
+        {
+            opcije[i].style.color='white';
+        }
+    }
+    else
+    {
+        navig.classList.remove("navigacija2");
+        for(var i=0;i<opcije.length;i++)
+        {
+            opcije[i].style.color='black';
+        }
+    }
+}        
